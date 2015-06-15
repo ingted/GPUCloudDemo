97e77b5aa61849b8b29647b727287c6d6b2053c0
Xiang Zhang <soloman817@gmail.com>
2015-06-15 21:18:01 +0800

    refactor calcpi

--- CloudScripts/CalcPI.fsx
+++ CloudScripts/CalcPI.fsx
@@ -50,14 +50,19 @@ let simulatePI rng seed numPointsLog numPointsPerStreamLog numStreamsPerTaskLog 
         |> Array.choose id
         |> Array.average
 
-    printfn "PI                     : %f" pi
+    printfn "PI                     : %.12f" pi
 
 let rngXorShift7 seed numStreams numDimensions = Rng.XorShift7.CUDA.DefaultUniformRandomModuleF64.Default.Create(numStreams, numDimensions, seed) :> Rng.IRandom<float>
 let rngMrg32k3a  seed numStreams numDimensions = Rng.Mrg32k3a.CUDA.DefaultUniformRandomModuleF64.Default.Create(numStreams, numDimensions, seed) :> Rng.IRandom<float>
 
-let numPointsLog = 35
-let numPointsPerStreamLog = 19
-let numStreamsPerTaskLog = 6
+// +2 because there are some workers that is cpu, so try to overlap a little, but this might not be good,
+// ideally, we should have all workers gpu enabled
+let numPointsLog = 32 + 2 
+// 23 results around 8 million points, that is roughly a good choice
+let numPointsPerStreamLog = 23
+// 5 results 32 streams per task, running this configure results tasks=64, which is good if your gpu
+// workers is less than 64 (I think mostly it is)
+let numStreamsPerTaskLog = 5
 let seed = 33u
 
 simulatePI rngMrg32k3a seed numPointsLog numPointsPerStreamLog numStreamsPerTaskLog
